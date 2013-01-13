da758ae3e467fad4aa480cbade6fd2f0345d1a49
Filip Procházka <hosiplan@gmail.com>
2013-01-13 20:59:00 +0100

    create composer package

--- examples/loader.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once __DIR__ . '/../../Nette/loader.php';
-require_once __DIR__ . '/../Wkhtmltopdf/IDocumentPart.php';
-require_once __DIR__ . '/../Wkhtmltopdf/Document.php';
-require_once __DIR__ . '/../Wkhtmltopdf/Page.php';
-require_once __DIR__ . '/../Wkhtmltopdf/PageMeta.php';
-require_once __DIR__ . '/../Wkhtmltopdf/Toc.php';
